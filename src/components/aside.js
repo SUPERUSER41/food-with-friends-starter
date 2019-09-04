9da6d016dcf98ed8278148ed8dee7a8a8edba80b
davidkartuzinski <davidk@kaidawei.com>
2019-09-04 09:04:19 +0200

    added Social Media Component

--- src/components/aside.js
+++ src/components/aside.js
@@ -3,6 +3,7 @@ import Bio from "./bio"
 import NewsletterSignup from "./newsletterSignup"
 import RecentEvents from "./recentEvents"
 import Instagram from "./instagram"
+import SocialMedia from "./socialMedia"
 import styled from "styled-components"
 import { themeAside } from "../styles"
 import { ThemeProvider } from "styled-components"
@@ -20,6 +21,7 @@ const Aside = () => {
       <ThemeProvider theme={themeAside}>
         <Instagram />
       </ThemeProvider>
+      <SocialMedia />
     </AsideHTML>
   )
 }
